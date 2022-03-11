6dc7c8af3ae8743bc82204ea0e27a22a8125816e
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-03-11 18:04:17 +0300

    feat: task finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let value = "look";
+    const result = str.split("");
+    let sum = result.length;
+    for (let i = 0; i < result.length; i++) {
+        for (let arr of bracketsConfig) {
+            if (arr.includes(result[i]) && result[i] === arr[0]) {
+                if (result[i + 1] === arr[1]) {
+                    result.splice(i, 2);
+                    value = "find";
+                    break;
+                } else {
+                    value = "look";
+                    break;
+                }
+            } else if (arr.includes(result[i]) && result[i] !== arr[0]) {
+                return false;
+            }
+        }
+        if (value === "find") {
+            i = -1;
+            continue;
+        }
+    }
+    return result.length === 0 ? true : false;
+};
